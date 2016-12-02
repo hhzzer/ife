5645beeef0c1c0e56b78130817e2a15e823fc69a
hhzzer <csdnmy@gmail.com>
2016-12-02 11:14:38 +0800

    finish one func

--- ife-task/task17/task17.js
+++ ife-task/task17/task17.js
@@ -9,19 +9,30 @@ var aqiSourceData = {
 };
 window.onload = function() {
 	var cubl=aqiSourceData["北京"];
-	console.log(cubl);
+	console.log(cubl instanceof Array);
 	//获取Object属性个数方法
-	console.log(Object.getOwnPropertyNames("北京").length);
-    //var cubl = [10, 20, 30, 30, 50, 100, 80];
-    var aqiChartWrap = document.getElementById('aqi-chart-wrap');
-    for (var i = 0; i < cubl.length; i++) {
+	var objectLenght = Object.getOwnPropertyNames("北京").length;
+	var aqiChartWrap = document.getElementById('aqi-chart-wrap');
+	for (var property in cubl){
+		console.log(property);
+		console.log(cubl[property]);
+		var cube = [];
+        cube[property] = document.createElement("div");
+        cube[property].setAttribute("class", "aqi-num");
+        cube[property].setAttribute("style", "height:" + cubl[property] + "px;width:" + 300 / objectLenght + "px;");
 
-        var cube = []
+        aqiChartWrap.appendChild(cube[property]);
+
+	} //var cubl = [10, 20, 30, 30, 50, 100, 80];
+    /*var aqiChartWrap = document.getElementById('aqi-chart-wrap');
+    for (var i = 0; i < objectLenght; i++) {
+
+        var cube = [];
         cube[i] = document.createElement("div");
         cube[i].setAttribute("class", "aqi-num");
         cube[i].setAttribute("style", "height:" + cubl[i] + "px;width:" + 300 / cubl.length + "px;");
 
         aqiChartWrap.appendChild(cube[i]);
         //console.log(typeof(cubl[i]));
-    }
+    }*/
 }
